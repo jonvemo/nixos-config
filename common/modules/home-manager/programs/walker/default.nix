5d99aa9a61bb66edd4f3c27b70c16bda2deee8d1
Jonvemo <rettouseisama@gmail.com>
2025-01-06 16:22:04 -0500

    style(walker): ident at default.nix for xdg.configFile

--- common/modules/home-manager/programs/walker/default.nix
+++ common/modules/home-manager/programs/walker/default.nix
@@ -1,5 +1,7 @@
 {
-  xdg.configFile."walker/config.json".source = ./config.json;
-  xdg.configFile."walker/themes/gtk.css".source = ./gtk.css;
-  xdg.configFile."walker/themes/gtk.json".source = ./gtk.json;
+  xdg.configFile = {
+    "walker/config.json".source = ./config.json;
+    "walker/themes/gtk.css".source = ./gtk.css;
+    "walker/themes/gtk.json".source = ./gtk.json;
+  };
 }
