3725a0c9d545600cafd6986a22f6add873c81486
Jonvemo <rettouseisama@gmail.com>
2025-01-17 12:34:06 -0500

    chore(hyprland): prefer .conf above .nix

--- common/modules/home-manager/programs/hyprland/default.nix
+++ common/modules/home-manager/programs/hyprland/default.nix
@@ -1,15 +1,15 @@
- {
-  imports = [
-    ./hyprland.nix
-    ./hyprvars.nix
-    ./modules/animations.nix
-    ./modules/controls.nix
-    ./modules/cursors.nix
-    ./modules/general.nix
-    ./modules/inputs.nix
-    ./modules/programs.nix
-    ./modules/rules.nix
-    ./modules/windows.nix
-    ./modules/workspaces.nix
-  ];
+{
+  xdg.configFile = {
+    "hypr/hyprland.conf".source = ./hyprland.conf;
+    "hypr/hyprvars.conf".source = ./modules/hyprvars.conf;
+    "hypr/modules/animations.conf".source = ./modules/animations.conf;
+    "hypr/modules/controls.conf".source = ./modules/controls.conf;
+    "hypr/modules/cursors.conf".source = ./modules/cursors.conf;
+    "hypr/modules/general.conf".source = ./modules/general.conf;
+    "hypr/modules/inputs.conf".source = ./modules/inputs.conf;
+    "hypr/modules/programs.conf".source = ./modules/programs.conf;
+    "hypr/modules/rules.conf".source = ./modules/rules.conf;
+    "hypr/modules/windows.conf".source = ./modules/windows.conf;
+    "hypr/modules/workspaces.conf".source = ./modules/workspaces.conf;
+  };
 }
