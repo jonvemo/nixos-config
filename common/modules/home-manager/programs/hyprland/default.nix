7179d6d1a953cb5ce6504bb010c35e5ddf74c7f1
Jonvemo <rettouseisama@gmail.com>
2025-01-29 10:02:27 -0500

    chore(hyprland): default.nix less verbose

--- common/modules/home-manager/programs/hyprland/default.nix
+++ common/modules/home-manager/programs/hyprland/default.nix
@@ -1,17 +1,18 @@
 { config, dotfiles, ... }: let
   symlink = config.lib.file.mkOutOfStoreSymlink;
+  path = dotfiles.programs;
 in {
   xdg.configFile = {
-    "hypr/hyprland.conf".source = symlink "${dotfiles.programs}/hyprland/hyprland.conf";
-    "hypr/hyprvars.conf".source = symlink "${dotfiles.programs}/hyprland/hyprvars.conf";
-    "hypr/modules/animations.conf".source = symlink "${dotfiles.programs}/hyprland/modules/animations.conf";
-    "hypr/modules/controls.conf".source = symlink "${dotfiles.programs}/hyprland/modules/controls.conf";
-    "hypr/modules/cursors.conf".source = symlink "${dotfiles.programs}/hyprland/modules/cursors.conf";
-    "hypr/modules/general.conf".source = symlink "${dotfiles.programs}hyprland/modules/general.conf";
-    "hypr/modules/inputs.conf".source = symlink "${dotfiles.programs}/hyprland/modules/inputs.conf";
-    "hypr/modules/programs.conf".source = symlink "${dotfiles.programs}/hyprland/modules/programs.conf";
-    "hypr/modules/rules.conf".source = symlink "${dotfiles.programs}/hyprland/modules/rules.conf";
-    "hypr/modules/windows.conf".source = symlink "${dotfiles.programs}/hyprland/modules/windows.conf";
-    "hypr/modules/workspaces.conf".source = symlink "${dotfiles.programs}/hyprland/modules/workspaces.conf";
+    "hypr/hyprland.conf".source = symlink "${path}/hyprland/hyprland.conf";
+    "hypr/hyprvars.conf".source = symlink "${path}/hyprland/hyprvars.conf";
+    "hypr/modules/animations.conf".source = symlink "${path}/hyprland/modules/animations.conf";
+    "hypr/modules/controls.conf".source = symlink "${path}/hyprland/modules/controls.conf";
+    "hypr/modules/cursors.conf".source = symlink "${path}/hyprland/modules/cursors.conf";
+    "hypr/modules/general.conf".source = symlink "${path}hyprland/modules/general.conf";
+    "hypr/modules/inputs.conf".source = symlink "${path}/hyprland/modules/inputs.conf";
+    "hypr/modules/programs.conf".source = symlink "${path}/hyprland/modules/programs.conf";
+    "hypr/modules/rules.conf".source = symlink "${path}/hyprland/modules/rules.conf";
+    "hypr/modules/windows.conf".source = symlink "${path}/hyprland/modules/windows.conf";
+    "hypr/modules/workspaces.conf".source = symlink "${path}/hyprland/modules/workspaces.conf";
   };
 }
