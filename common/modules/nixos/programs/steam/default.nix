247dc49477efe2a5de385961c7fb0a6c7c0dee2f
Jonvemo <rettouseisama@gmail.com>
2025-02-01 22:36:54 -0500

    chore(steam): remove extracompactpackage proton-ge-bin

--- common/modules/nixos/programs/steam/default.nix
+++ common/modules/nixos/programs/steam/default.nix
@@ -5,6 +5,5 @@
     gamescopeSession.enable = true;
     remotePlay.openFirewall = false;
     dedicatedServer.openFirewall = false;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 }
