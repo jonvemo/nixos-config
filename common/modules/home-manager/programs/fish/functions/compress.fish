c7b7dbbec96075ab60b7bc420dfc7e9da283bfa4
Jonvemo <rettouseisama@gmail.com>
2025-02-03 15:14:47 -0500

    refactor(fish): enhance compress function with better input validation

--- common/modules/home-manager/programs/fish/functions/compress.fish
+++ common/modules/home-manager/programs/fish/functions/compress.fish
@@ -2,11 +2,22 @@
 
 function compress
     if test (count $argv) -ne 2
-        echo "Use: compress <compress_file> <folder_to_compress>"
+        echo "Use: compress <output_name> <folder_to_compress>"
         return 1
     end
-    set FILE $argv[1].tar.xz
-    set FOLDER $argv[2]
-    tar -cJf $FILE $FOLDER
-    echo "Folder '$FOLDER' has been compressed to '$FILE'"
+
+    set FILE "$argv[1].tar.xz"
+    set FOLDER "$argv[2]"
+
+    if not test -d "$FOLDER"
+        echo "Error: Folder '$FOLDER' does not exist."
+        return 1
+    end
+
+    if tar -cJf "$FILE" "$FOLDER"
+        echo "Folder '$FOLDER' has been compressed to '$FILE'"
+    else
+        echo "Error: Failed to compress '$FOLDER'."
+        return 1
+    end
 end
