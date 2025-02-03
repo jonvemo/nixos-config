504be9cecca408d86674c891fe2b1cd20f5c1d05
Jonvemo <rettouseisama@gmail.com>
2025-02-03 16:16:10 -0500

    refactor(fish): improve create_module function with input validation and auto-import

--- common/modules/home-manager/programs/fish/functions/create_module.fish
+++ common/modules/home-manager/programs/fish/functions/create_module.fish
@@ -1,6 +1,20 @@
 #!/usr/bin/env fish
 
 function create_module
+    if test (count $argv) -ne 1
+        echo "Usage: create_module <module>"
+        return 1
+    end
+
     set -l NAME $argv[1]
-    mkdir $NAME && cd $NAME && touch default.nix && hx default.nix
+
+    mkdir -p $NAME
+    touch $NAME/default.nix
+
+    # Add the new module to imports
+    sed -i "/imports = \[/,/\]/ {
+        /\]/i \    ./$NAME
+    }" default.nix
+
+    hx $NAME/default.nix
 end
