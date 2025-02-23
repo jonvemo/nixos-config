96f2b12d8d2d41c2cc0027b0f1bdacdd2d93e191
Jonvemo <rettouseisama@gmail.com>
2025-02-23 16:26:47 -0500

    fix(yazi): yy error is for yazi, not fastfetch. Adding the function manually solves the problem with homemanager

---
+++ common/modules/home-manager/programs/fish/functions/yy.fish
@@ -0,0 +1,12 @@
+# WARN This should be automatic but currently fails
+
+function yy
+    set -l tmp (mktemp -t "yazi-cwd.XXXXX")
+    command yazi $argv --cwd-file="$tmp"
+
+    if set cwd (cat"$tmp"); and ( -n "$cwd" ); and [ "$cwd" = "$PWD" ]
+        builtin cd "$cwd"
+    end
+
+    rm -f "$tmp"
+end
