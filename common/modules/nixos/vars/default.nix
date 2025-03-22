9a281e4e9a4cad74b8a7b0be69f0ad753df1bfc7
Jonvemo <rettouseisama@gmail.com>
2025-03-22 15:20:41 -0500

    feat(kooha): enable experimental features

--- common/modules/nixos/vars/default.nix
+++ common/modules/nixos/vars/default.nix
@@ -2,6 +2,8 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     EDITOR = "hx";
+    # NOTE More info: https://github.com/SeaDve/Kooha?tab=readme-ov-file#%EF%B8%8F-experimental-features
+    KOOHA_EXPERIMENTAL = "all";
   };
   
 }
