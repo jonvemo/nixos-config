94b0d266a45d11c064c1ad56b2bc5da3167bac87
Jonvemo <rettouseisama@gmail.com>
2025-04-10 00:54:26 -0500

    chore(zed): add providers and models list, change ai at inline-alternative and add ai new options

--- home/jonvemo/modules/programs/zed/assistant.nix
+++ home/jonvemo/modules/programs/zed/assistant.nix
@@ -1,17 +1,47 @@
-{
+# NOTE
+#   Preferred Providers and Models
+#     zed.dev      :    claude-3-7-sonnet-latest, claude-3-7-sonnet-thinking-latest
+#     google       :    gemini-2.0-flash-thinking-exp, gemini-2.5-pro-exp-03-25, gemini-2.0-flash
+#     copilot_chat :    gpt-4o, gpt-4
+#     ollama       :    deepseek-r1:1.5b, deepseek-r1:7b
+#
+
+let
+  main-provider = "google";
+  main-model = "gemini-2.0-flash";
+  main-reasoner-model = "gemini-2.0-flash-thinking-exp";
+  
+in {
   enabled = true;
   default_width = 384;
   dock = "right";
   version = "2";
 
   default_model = {
-    provider = "google";            # ollama
-    model = "gemini-2.0-flash-thinking-exp"; # deepseek-r1:1.5b/7b
+    provider = main-provider;
+    model = main-reasoner-model;
   };
+  editor_model = {
+    provider = main-provider;
+    model = main-reasoner-model;
+  };
+  inline_assistant_model = {
+    provider = main-provider;
+    model = main-model;
+  };
+  commit_message_model = {
+    provider = main-provider;
+    model = main-model;
+  };
+  thread_summary_model = {
+    provider = main-provider;
+    model = main-model;
+  };
+  
   inline_alternatives = [
     {
-      provider = "openai";
-      model = "o3-mini";
+      provider = "zed.dev";
+      model = "claude-3-7-sonnet-lastest";
     }
   ];
   
