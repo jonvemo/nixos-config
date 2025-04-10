9dd4311924cbef55a58dc65883c3df48b0c855ce
Jonvemo <rettouseisama@gmail.com>
2025-04-10 16:48:53 -0500

    chore(ollama): disable

--- common/modules/nixos/services/ollama/default.nix
+++ common/modules/nixos/services/ollama/default.nix
@@ -1,15 +1,11 @@
-{pkgs,...}:
 {
   services.ollama = {
-    enable = true;
+    enable = false;
+    
     loadModels = [
       "deepseek-r1:1.5b"
       "deepseek-r1:7b"
     ];
+    
   };
-  
-  # environment.systemPackages = with pkgs; [
-  #   /* GUI for Ollama */
-  #   alpaca
-  # ];
 }
