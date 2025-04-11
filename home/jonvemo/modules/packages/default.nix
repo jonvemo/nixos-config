65777fd51f2e54b648ad9244b806e69af60b54c2
Jonvemo <rettouseisama@gmail.com>
2025-04-10 23:22:57 -0500

    chore(remnote): remove

--- home/jonvemo/modules/packages/default.nix
+++ home/jonvemo/modules/packages/default.nix
@@ -4,23 +4,12 @@
   home.packages = lib.concatLists [ 
   
     (with pkgs; [
-    
-      # Programming
-  
-      # Learning
-      remnote
-
-      # Desing
       figma-linux
-
+    
     ])
      
-    # Last Update (nixos-unstable-small)
     (with pkgs-small; [
-
-      # Programming
       lazygit woff2
-      # Temporal; in case an unstable package (nixos-unstable) fails
     
     ])  
   ];
