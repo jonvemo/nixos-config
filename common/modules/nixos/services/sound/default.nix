c4d16d5d4d0abb30f083a2cb29c75ee7d8f674e5
Jonvemo <rettouseisama@gmail.com>
2025-04-11 01:37:41 -0500

    chore(sound): clean

--- common/modules/nixos/services/sound/default.nix
+++ common/modules/nixos/services/sound/default.nix
@@ -1,22 +1,12 @@
-{ lib, ... }:
 {
-  services = {
-    pipewire = {
-      enable = true;
-      wireplumber.enable = true;
+  services.pipewire = {
+    enable = true;
 
-      /* Compatibility - All can run only with this, restart required. Wayland */
-      pulse.enable = true;
+    /* Compatibility - All can run only with this, restart required. Wayland */
+    pulse.enable = true;
 
-      /* Specific Use */
-      jack.enable = false;
-
-      /* Enable Main Device Sound and a lot of Packages */
-      alsa = {
-        enable = true; # WARN osu!lazer required
-        support32Bit = lib.mkDefault false;
-      };
-    };
+    /* Legacy - osu!lazer */
+    alsa.enable = true;
   };
   
 }
