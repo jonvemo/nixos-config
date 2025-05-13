a42582347822eb84782275da8c00847b057aba5f
Jonvemo <rettouseisama@gmail.com>
2025-05-12 20:27:14 -0500

    feat(zzz): add zenless zone zero launcher
    Remove General Launcher for Hoyoverse Games

--- common/modules/nixos/programs/default.nix
+++ common/modules/nixos/programs/default.nix
@@ -8,7 +8,7 @@
     ./nautilus
     ./curd
     ./steam
-    ./anime-games
+    ./zzz
   ];
 
   programs = {
--- common/modules/nixos/programs/zzz/default.nix
+++ common/modules/nixos/programs/zzz/default.nix
@@ -3,5 +3,5 @@
   imports = [ inputs.aagl.nixosModules.default ];
   nix.settings = inputs.aagl.nixConfig; # Set Up Cachix
   
-  programs.anime-games-launcher.enable = true;
+  programs.sleepy-launcher.enable = true;
 }
