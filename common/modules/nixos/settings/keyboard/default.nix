e33e4ac8c769a2eef5dc4021daca0506827364c6
Jonvemo <rettouseisama@gmail.com>
2025-05-21 15:44:41 -0500

    refactor(fcitx5): tabs and enable wayland frontend

--- common/modules/nixos/settings/keyboard/default.nix
+++ common/modules/nixos/settings/keyboard/default.nix
@@ -1,11 +1,17 @@
 { pkgs, ... }:
 {
   i18n.inputMethod = {
-   type = "fcitx5";
-   enable = true;
-   fcitx5.addons = with pkgs; [
-     fcitx5-mozc
-     fcitx5-gtk
-   ];
- };
+    enable = true;
+    type = "fcitx5";
+  
+    fcitx5 = {
+      waylandFrontend = true;
+    
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-mozc
+      ];
+    };
+  
+  };
 }
