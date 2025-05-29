f31242132ae5bb831dcda800ba6da7d14d3913c1
Jonvemo <rettouseisama@gmail.com>
2025-05-29 13:51:00 -0500

    chore(zed): remove flake

--- home/jonvemo/modules/programs/zed/default.nix
+++ home/jonvemo/modules/programs/zed/default.nix
@@ -1,17 +1,16 @@
-{pkgs, inputs, system, ...}: {
-    
+{pkgs,...}:
+{
   programs.zed-editor = {
     enable = true;
-    package = inputs.zed-editor.packages.${system}.zed-editor-bin;
 
     extensions = [
       "html" "nix" "fish" "toml"
       "biome" "liveserver" "emmet" "marksman" "markdown-oxide"
       "log" "git-firefly"
     ];
-    
+
     extraPackages = with pkgs; [
-     nixd nil
+      nixd nil
     ];
 
     userSettings = {
