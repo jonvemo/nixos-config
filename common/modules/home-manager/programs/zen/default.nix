c4aa87b72c0da1287706470c6d00d63887a61c38
Jonvemo <rettouseisama@gmail.com>
2025-06-01 11:12:16 -0500

    chore(zen-browser): add more policies settings

--- common/modules/home-manager/programs/zen/default.nix
+++ common/modules/home-manager/programs/zen/default.nix
@@ -6,16 +6,65 @@
     enable = true;
     
     policies = {
+      DisplayBookmarksToolbar = "newtab";
       NoDefaultBookmarks = true;
       DisableFirefoxScreenshots = true;
       DisableSetDesktopBackground = true;
       DisableFeedbackCommands = true;
       DisablePocket = true;
-      DisplayBookmarksToolbar = "newtab";
+      DisableFirefoxAccounts = true;
+      DisableFirefoxStudies = true;
+      DisableTelemetry = true;
+      DisableAppUpdate = true;
+      BlockAboutSupport = true;
+      DontCheckDefaultBrowser = true;
+      OfferToSaveLogins = false;
+      HttpsOnlyMode = "enabled";
+
+      Preferences = {
+        "browser.translations.automaticallyPopup" = {
+          Value = false;
+          Status = "user";
+        };
+      };
+
+      SanatizeOnShutdown = {
+        Cache = true;
+      };
+
+      Cookies = {
+        Behavior = "reject-tracker-and-partition-foreign";
+      };
 
       ExtensionSettings = {
         "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           private_browsing = true;
+        };
+      };
+
+      "3rdparty" = {
+        Extensions = {
+          "uBlock0@raymondhill.net" = {
+            userSettings = [
+              [ "contextMenuEnabled" "false" ]
+              [ "ignoreGenericCosmeticFilters" "true" ]
+            ];
+            toOverwrite = {
+              filerLists = [
+                "ublock-privacy"
+                "ublock-badware"
+                "ublock-filters"
+              ];
+            };
+            disabledPopupPanelParts = [
+              "basicTools"
+              "extraTools"
+              "overviewPane"
+            ];
+            
+          };
         };
       };
       
