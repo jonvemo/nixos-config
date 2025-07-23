6901d561deeec72d662d9871d08cdd40124c7e54
Jonvemo <rettouseisama@gmail.com>
2025-07-23 16:50:26 -0500

    chore(graphics): remove comment about vlk

--- common/modules/nixos/hardware/graphics/default.nix
+++ common/modules/nixos/hardware/graphics/default.nix
@@ -1,9 +1,3 @@
 {
-  hardware = {
-    graphics.enable = true;
-
-    # NOTE Enable "xserver" too
-      # https://github.com/NixOS/nixpkgs/blob/7eee17a8a5868ecf596bbb8c8beb527253ea8f4d/nixos/modules/services/hardware/amdvlk.nix
-    # amdgpu.amdvlk.enable = true;
-  };
+  hardware.graphics.enable = true;
 }
