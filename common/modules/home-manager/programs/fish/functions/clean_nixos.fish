2d6b2e48f0a4c57d49259978e7c935b4ed2dc6e0
Jonvemo <rettouseisama@gmail.com>
2025-07-25 18:19:57 -0500

    chore(fish): remove clean_nixos.fish

--- common/modules/home-manager/programs/fish/functions/clean_nixos.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-function clean_nixos
-    nix-env --delete-generations old
-    sudo nix-collect-garbage -d
-    sudo nix-store --optimise
-    sudo nixos-rebuild switch --flake /etc/nixos#jonvemo
-    # nh os switch --ask /etc/nixos
-end
