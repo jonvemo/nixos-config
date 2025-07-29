ba69fd6dbe6f25d264cf06d5417e31c9a540e69f
Jonvemo <rettouseisama@gmail.com>
2025-07-29 00:03:25 -0500

    chore(font): remove dejavufont

--- common/modules/nixos/settings/font/default.nix
+++ common/modules/nixos/settings/font/default.nix
@@ -12,7 +12,6 @@
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
         noto-fonts-emoji-blob-bin
-        dejavu_fonts
         
       ])
       
@@ -33,9 +32,9 @@
 
       defaultFonts = {
         emoji = [ "Blobmoji" ];
-        monospace = [ "JetBrainsMono Nerd Font" "DejaVu Sans Mono"];
-        serif = [ "Noto Serif" "Noto Serif Japanese" "Noto Serif Korean" "Noto Serif Chinese" "DejaVu Serif"];
-        sansSerif = [ "Nunito" "Noto Sans" "Noto Sans Japanese" "Noto Sans Korean" "Noto Sans Chinese" "DejaVu Sans"];
+        monospace = [ "JetBrainsMono Nerd Font"];
+        serif = [ "Noto Serif" "Noto Serif Japanese" "Noto Serif Korean" "Noto Serif Chinese"];
+        sansSerif = [ "Nunito" "Noto Sans" "Noto Sans Japanese" "Noto Sans Korean" "Noto Sans Chinese"];
       };
     };
     
