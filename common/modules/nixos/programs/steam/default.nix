a537581e0317bd136e0313d477e4a9f49a0f2967
Jonvemo <rettouseisama@gmail.com>
2025-08-09 18:42:46 -0500

    feat(steam): enable gamescope

--- common/modules/nixos/programs/steam/default.nix
+++ common/modules/nixos/programs/steam/default.nix
@@ -2,7 +2,7 @@
 {
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = false;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = false;
     dedicatedServer.openFirewall = false;
   };
