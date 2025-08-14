7d3ecf2e838483bdb57fd2ef18ba0509cf0d00d1
Jonvemo <rettouseisama@gmail.com>
2025-08-13 21:26:22 -0500

    feat(nix): enable use-xdg-base-directories

--- common/modules/nixos/system/nix/default.nix
+++ common/modules/nixos/system/nix/default.nix
@@ -2,6 +2,7 @@
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
+      use-xdg-base-directories = true;
       trusted-users = [ "root" "jonvemo" ];
       substituters = import ./substituters;
       trusted-public-keys = import ./trusted-public-keys;
