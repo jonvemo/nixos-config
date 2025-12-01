e8017e9bf8331bf33ecbcdb617e7a04568b1145a
Jonvemo <rettouseisama@gmail.com>
2025-12-01 03:37:01 -0500

    chore(niri): update niri config for new release

--- common/modules/home-manager/programs/niri/default.nix
+++ common/modules/home-manager/programs/niri/default.nix
@@ -4,5 +4,8 @@
 in {
   xdg.configFile = {
     "niri/config.kdl".source = symlink "${path}/niri/niri.kdl";
+    "niri/binds.kdl".source = symlink "${path}/niri/binds.kdl";
+    "niri/rules.kdl".source = symlink "${path}/niri/rules.kdl";
+    "niri/misc.kdl".source = symlink "${path}/niri/misc.kdl";
   };
 }
