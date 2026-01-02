816220120b475c46d6a5d57bbcafcac9f9f62b6d
Jonvemo <rettouseisama@gmail.com>
2026-01-02 18:32:39 -0500

    feat(yazi): add music opener with kew

--- common/modules/nixos/programs/yazi/default.nix
+++ common/modules/nixos/programs/yazi/default.nix
@@ -7,9 +7,13 @@
           sort_sensitive = true;
           show_symlink = true;
         };
+        opener.music = [
+          { run = "footclient -T kew kew $(basename %s)"; orphan = true; for = "linux"; }
+        ];
         open.rules = [
           { mime = "text/*"; use = "edit"; }
           { mime = "application/*"; use = "edit"; }
+          { mime = "audio/*"; use = "music"; }
           { mime = "*"; use = "open"; }
         ];
       };
