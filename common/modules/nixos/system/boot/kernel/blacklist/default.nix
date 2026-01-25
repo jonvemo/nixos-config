b0cef46478b1fef0508865551bdea6101ea15196
Jonvemo <rettouseisama@gmail.com>
2026-01-25 14:36:50 -0500

    feat(kernel): add joydev, libceph, af_key and irda modules in blacklist

--- common/modules/nixos/system/boot/kernel/blacklist/default.nix
+++ common/modules/nixos/system/boot/kernel/blacklist/default.nix
@@ -20,6 +20,8 @@
     "p8023"     # Novell raw IEEE 802.3
     "can"       # Controller Area Network
     "atm"       # ATM
+    "af_key"    # PF_KEY socket family (Legacy IPsec)
+    "irda"      # Infrared Data Association (Legacy Wireless)
 
     #           Old or rare or insufficiently audited filesystems
     "adfs"      # Active Directory Federation Services
@@ -33,24 +35,28 @@
     "exofs"     # EXtended Object File System
     "freevxfs"  # Veritas filesystem driver
     "f2fs"      # Flash-Friendly File System
-    "vivid"     # Virtual Video Test Driver (unnecessary, and a historical cause of escalation issues)
+    "vivid"     # Virtual Video Test Driver
     "gfs2"      # Global File System 2
     "hpfs"      # High Performance File System (used by OS/2)
     "hfs"       # Hierarchical File System (Macintosh)
     "hfsplus"   # " same as above, but with extended attributes
     "jffs2"     # Journalling Flash File System (v2)
-    "jfs"       # Journaled File System - only useful for VMWare sessions
+    "jfs"       # Journaled File System
     "ksmbd"     # SMB3 Kernel Server
-    "minix"     # minix fs - used by the minix OS
-    "nfsv3"     # " (v3)
+    "minix"     # minix fs
+    "nfsv3"     # Network File System (v3)
     "nfsv4"     # Network File System (v4)
     "nfs"       # Network File System
     "nilfs2"    # New Implementation of a Log-structured File System
     "omfs"      # Optimized MPEG Filesystem
-    "qnx4"      # extent-based file system used by the QNX4 and QNX6 OSes
+    "qnx4"      # extent-based file system (QNX4/QNX6)
     "qnx6"      # "
-    "squashfs"  # compressed read-only file system (used by live CDs)
-    "sysv"      # implements all of Xenix FS, SystemV/386 FS and Coherent FS.
-    "udf"       # https://docs.kernel.org/5.15/filesystems/udf.html
+    "squashfs"  # compressed read-only file system
+    "sysv"      # Xenix, SystemV/386 and Coherent FS
+    "udf"       # Universal Disk Format
+
+    #           Input and modern vulnerabilities
+    "joydev"    # Legacy Joystick device interface
+    "libceph"   # Ceph Network Protocol
   ];
 }
