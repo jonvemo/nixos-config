c021964c585bcf1fb6ad183351e3c3b73f0680dc
Jonvemo <rettouseisama@gmail.com>
2026-02-03 13:56:32 -0500

    chore(kitty): restore some default options

--- common/modules/home-manager/programs/terminal/kitty/default.nix
+++ common/modules/home-manager/programs/terminal/kitty/default.nix
@@ -5,18 +5,16 @@
     settings = {
       confirm_os_window_close = 0; 
       allow_remote_control = "yes";
-      scrollback_lines = 10000;
       copy_on_select = "yes";
       enabled_layouts = "grid";
-      window_border_width = "2pt";
+      window_border_width = "0.5pt";
       window_padding_width = "2";
-      window_margin_width = "0";
-      draw_minimal_borders = "no";
+      window_margin_width = "2";
+      draw_minimal_borders = "yes";
       bell_on_tab = "yes";
       tab_bar_style = "powerline";
       tab_activity_symbol = "😼";
       tab_title_template = " {title[title.rfind('/')+1:]} {tab.active_exe} {activity_symbol}";
-      background_blur = "8";
     };
 
     keybindings = {
