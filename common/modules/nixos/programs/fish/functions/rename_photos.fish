01fab5a2a8f1da66a377c8343f412cde0b033ed3
Jonvemo <rettouseisama@gmail.com>
2026-02-14 16:27:40 -0500

    refactor(fish): rename_photos function

--- common/modules/nixos/programs/fish/functions/rename_photos.fish
+++ common/modules/nixos/programs/fish/functions/rename_photos.fish
@@ -1,26 +1,18 @@
 #!/usr/bin/env fish
 
 function rename_photos
-    set IMAGE_FORMATS jpg jpeg png gif webp avif heic tiff bmp
-    set COUNTER 1
-    set FOUND_FILES false
-
-    for FORMAT in $IMAGE_FORMATS
-        for FILE in *.$FORMAT
-            set FOUND_FILES true
-            set EXIF_DATE (exiv2 -g Exif.Photo.DateTimeOriginal -Pt "$FILE" 2>/dev/null | string replace -a ':' '-' | string replace ' ' '_')
-
-            if test -z "$EXIF_DATE"
-                set EXIF_DATE (stat --format='%y' $FILE | cut -d'.' -f1 | sed 's/[:\t]/ /g' | awk '{print $1" "$2$3$4}')
-            end
-
-            mv -v "$FILE" "Y2K-$COUNTER $EXIF_DATE.$FORMAT"
-            set COUNTER (math $COUNTER + 1)
-        end
-    end
-
-    if not $FOUND_FILES
+    set FILES *.{jpg,jpeg,png,gif,webp,avif,heic}
+    test -n "$FILES[1]"; or begin
         echo "Error: files not found"
         return 1
+    end
+
+    for i in (seq (count $FILES))
+        set F $FILES[$i]
+        set EXT (string split -r -m1 . $F)[2]
+        set DATE (exiv2 -g Exif.Photo.DateTimeOriginal -Pt "$F" 2>/dev/null | string replace -a ':' '-' | string replace ' ' '_')
+        test -n "$DATE"; or set DATE (date -r "$F" "+%Y-%m-%d %H%M%S")
+
+        mv -v "$F" "Y2K-$i $DATE.$EXT"
     end
 end
