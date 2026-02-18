5e94ca9d05ee48205fc694d9ab234f81a21e8c0f
Jonvemo <rettouseisama@gmail.com>
2026-02-18 14:54:43 -0500

    fix(fonts): fonts names

--- common/modules/nixos/settings/font/default.nix
+++ common/modules/nixos/settings/font/default.nix
@@ -1,29 +1,25 @@
 { lib, pkgs, ... }:
-
 {
   fonts = {
     enableDefaultPackages = false;
 
-    packages = lib.concatLists [ 
-    
+    packages = lib.concatLists [
+
       (with pkgs; [
         noto-fonts-lgc-plus
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
         noto-fonts-emoji-blob-bin
-        
       ])
-      
+
       (with pkgs.nerd-fonts; [
         jetbrains-mono
-        
       ])
 
       (with pkgs.texlivePackages; [
         nunito
-        
       ])
-      
+
     ];
 
     fontconfig = {
@@ -31,11 +27,23 @@
 
       defaultFonts = {
         emoji = [ "Blobmoji" ];
-        monospace = [ "JetBrainsMono Nerd Font"];
-        serif = [ "Noto Serif" "Noto Serif Japanese" "Noto Serif Korean" "Noto Serif Chinese"];
-        sansSerif = [ "Nunito" "Noto Sans" "Noto Sans Japanese" "Noto Sans Korean" "Noto Sans Chinese"];
+        monospace = [ "JetBrainsMono Nerd Font" ];
+        serif = [
+          "Noto Serif"
+          "Noto Serif CJK JP"
+          "Noto Serif CJK KR"
+          "Noto Serif CJK SC"
+          "Noto Serif CJK TC"
+        ];
+        sansSerif = [
+          "Nunito"
+          "Noto Sans"
+          "Noto Sans CJK JP"
+          "Noto Sans CJK KR"
+          "Noto Sans CJK SC"
+          "Noto Sans CJK TC"
+        ];
       };
     };
-    
   };
 }
