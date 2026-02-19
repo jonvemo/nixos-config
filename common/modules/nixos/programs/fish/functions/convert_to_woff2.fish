146040884e6f5ea340521d4c4efe5877fed7d1ca
Jonvemo <rettouseisama@gmail.com>
2026-02-18 19:41:18 -0500

    refactor(fish): simplify convert_to_woff2 function

--- common/modules/nixos/programs/fish/functions/convert_to_woff2.fish
+++ common/modules/nixos/programs/fish/functions/convert_to_woff2.fish
@@ -1,20 +1,9 @@
 #!/usr/bin/env fish
 
 function convert_to_woff2
-    set -l FOLDER fonts
-
-    # Create the folder if it doesn't exist
-    mkdir -p $FOLDER
-
-    # Find all files in the current directory with the .ttf extension
-    for FILE in *.ttf
-        # Apply woff2_compress to the file
-        woff2_compress "$FILE"
-
-        # Change the file extension to .woff2
-        set FILE_COMPRESSED (string replace ".ttf" ".woff2" -- $FILE)
-
-        # Move the compressed file to the destination folder
-        mv "$FILE_COMPRESSED" $FOLDER
+    mkdir -p fonts
+    for file in *.ttf
+        woff2_compress $file
     end
+    mv *.woff2 fonts/
 end
