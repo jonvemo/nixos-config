338e6897edd7651de076ae2b89b744a26c6af8d2
Jonvemo <rettouseisama@gmail.com>
2026-03-15 13:15:22 -0500

    chore(fish): optimize rename_photos function with regex and zero-padding

--- common/modules/nixos/programs/fish/functions/rename_photos.fish
+++ common/modules/nixos/programs/fish/functions/rename_photos.fish
@@ -2,17 +2,21 @@
 
 function rename_photos
     set FILES *.{jpg,jpeg,png,gif,webp,avif,heic}
-    test -n "$FILES[1]"; or begin
-        echo "Error: files not found"
-        return 1
-    end
+    set -q FILES[1]; or return 1
+
+    set PADDING (string length (count $FILES))
 
     for i in (seq (count $FILES))
         set F $FILES[$i]
         set EXT (string split -r -m1 . $F)[2]
-        set DATE (exiv2 -g Exif.Photo.DateTimeOriginal -Pt "$F" 2>/dev/null | string replace -a ':' '-' | string replace ' ' '_')
-        test -n "$DATE"; or set DATE (date -r "$F" "+%Y-%m-%d %H%M%S")
 
-        mv -v "$F" "Y2K-$i $DATE.$EXT"
+        set RAW (exiv2 -g Exif.Photo.DateTimeOriginal -Pt "$F" 2>/dev/null; or date -r "$F" "+%Y:%m:%d %H:%M:%S")
+
+        set DATE (string replace -r '(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})' '$1-$2-$3 $4$5$6' "$RAW")
+
+        set INDEX (string pad -w $PADDING -c 0 $i)
+        set NEW "Y2K-$INDEX $DATE.$EXT"
+
+        test "$F" != "$NEW"; and mv -v "$F" "$NEW"
     end
 end
