2e68afe49a930334a24e4d3446e31a1948adc776
Jonvemo <rettouseisama@gmail.com>
2026-04-01 17:47:26 -0500

    refactor(locale): remove extra settings

--- common/modules/nixos/settings/locale/default.nix
+++ common/modules/nixos/settings/locale/default.nix
@@ -1,14 +1,3 @@
 {
   i18n.defaultLocale = "es_CO.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_CO.UTF-8";
-    LC_IDENTIFICATION = "es_CO.UTF-8";
-    LC_MEASUREMENT = "es_CO.UTF-8";
-    LC_MONETARY = "es_CO.UTF-8";
-    LC_NAME = "es_CO.UTF-8";
-    LC_NUMERIC = "es_CO.UTF-8";
-    LC_PAPER = "es_CO.UTF-8";
-    LC_TELEPHONE = "es_CO.UTF-8";
-    LC_TIME = "es_CO.UTF-8";
-  };
 }
